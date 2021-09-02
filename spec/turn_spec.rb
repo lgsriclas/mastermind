c4bc362b1cc647174e47398a20c8046965e083d7
Stephanie Helm <stephaniemhelm@gmail.com>
2021-09-02 13:24:47 -0600

    test: add index and correct element tests

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -10,5 +10,22 @@ RSpec.describe Turn do
     expect(a).to be_a(Integer)
   end
 
+  it 'returns correct index position' do
+    turn = Turn.new
+    a = turn.evaluate_index(["r", "g", "g", "b"], ["y", "r", "b", "g"])
+    expect(a).to be_a(0)
+
+    b = turn.evaluate_index(["r", "g", "g", "b"], ["r", "r", "b", "b"])
+    expect(b).to be_a(2)
+  end
+
+  it 'returns correct element number' do
+    turn = Turn.new
+    a = turn.evaluate_method(["r", "g", "g", "b"], ["y", "r", "b", "g"])
+    expect(a).to be_a(3)
+
+    b = turn.evaluate_method(["r", "g", "g", "b"], ["r", "r", "b", "b"])
+    expect(b).to be_a(2)
+  end
 
 end
