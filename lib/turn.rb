1fb3d586fae16c0715fb847e37cdcb20870f4a08
Lesley Sanders <lesleyasanders@gmail.com>
2021-09-05 12:30:19 -0400

    Fix: update secret class

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,22 +16,16 @@ class Turn
       "Guess is too short, try again!"
     elsif guess_to_array(guess).length > 4
       "Guess is too long, try again!"
-<<<<<<< HEAD
-    elsif guess_to_array(guess).delete("r", "g", "b", "y").any? == true
-=======
     elsif guess_to_array(guess).delete(["r", "g", "b", "y"]).any? == true
->>>>>>> f706b9b75b9ca14f4a6745e04adaab76b5e33bbc
       "Your input had invalid characters."
     else
       # whatever method comes next!!!
     end
   end
-<<<<<<< HEAD
 
-  #def guess_iteration(guess)
-
-#  end
-
+#   def guess_iteration(guess)
+#   end
+#
 #secret code = rggb  guess = yrbg
 #element loop and index loop
   def evaluate_index(secret_code, guess)
@@ -43,31 +37,6 @@ class Turn
 
   end
 
-  def evaluate_element(secret_code, guess)
-    #evaluates elements
-  end
-
-  def evaluate_method
-    #call previous methods and return amount correct for both
-
-  end
-=======
-
-#   def guess_iteration(guess)
-#
-#   end
-#
-# #secret code = rggb  guess = yrbg
-# #element loop and index loop
-#   def evaluate_index(secret_code, guess)
-#     #compare index postions
-#     #array.each_with_index {|val, index|}
-#     #does this evaluate both at the same time?
-#
-#
-#
-#   end
-#
 #   def evaluate_element(secret_code, guess)
 #     #evaluates elements
 #   end
@@ -76,6 +45,5 @@ class Turn
 #     #call previous methods and return amount correct for both
 #
 #   end
->>>>>>> f706b9b75b9ca14f4a6745e04adaab76b5e33bbc
 
 end
---
+++ runner/mastermind.rb
@@ -0,0 +1,7 @@
+require './lib/game'
+
+
+p "Welcome to MASTERMIND! Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+
+game = Game.new
+game.start
