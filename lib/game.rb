68542816fb65858192fe7d43cb9c4ed8909bbaad
Lesley Sanders <lesleyasanders@gmail.com>
2021-09-05 13:50:29 -0400

    Fix: update time methods

--- lib/game.rb
+++ lib/game.rb
@@ -3,11 +3,14 @@ require './lib/secret'
 require './lib/turn'
 
 class Game
-  attr_reader :i, :q, :c, :turn_counter, :secret
+  attr_reader :turn_counter, :secret, :user_input, :start_time, :end_time, :player_guess, :game_time
   def initialize
+    @guess_counter = 0
+    @secret = []
     @player_guess = player_guess
-    @turn_counter = 0
-    @secret = secret
+    @start_time = start_time
+    @end_time = end_time
+    @game_time = game_time
   end
 
   def instructions
@@ -18,9 +21,8 @@ class Game
     "You have quit Mastermind"
   end
 
-
   def start_game
-    user_input = gets.chomp
+    player_input = gets.chomp.downcase
     if user_input == "p"
       play_game
     elsif user_input == "i"
@@ -32,13 +34,27 @@ class Game
     elsif user_input == "c"
       p cheat
     end
+    player_input
+  end
+
+  def set_start_time
+    @start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  end
+
+  def set_end_time
+    @end_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  end
+
+  def calculate_game_time
+    #game_time = distance_of_time_in_words(set_start_time, set_end_time)
+    @game_time = (set_end_time - set_start_time).round(2)
   end
 
   def play_game
     set_secret_code
-    turn = Turn.new
-    p  "I have generated a beginner sequence with four elements made up of: (r)ed,
-          (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game. What's your guess?"
+    set_start_time
+    #turn = Turn.new
+    p  "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game. What's your guess?"
   end
 
   def set_secret_code
@@ -50,20 +66,24 @@ class Game
     @secret.secret_code
   end
 
-  def guesses(guess)
-    guess.each do |guess|
-      @turn_counter += 1
-    end
+  def guess
+    @player_guess = gets.chomp.downcase
+    add_guess
+  end
+
+  def add_guess
+    @guess_counter += 1
   end
 
   def end_game
+    set_end_time
     if @guess == @secret_code
       p end_game_message
     end
   end
 
   def end_game_message
-    p "Congratulations! You guessed the sequence '#{set_secret_code}' in #{guesses} guesses over #{minutes} minutes,
-      #{seconds} seconds. Do you want to (p)lay again or (q)uit?"
+    p "Congratulations! You guessed the sequence '#{set_secret_code}' in #{turn_counter} guesses over #{calculate_game_time}. Do you want to (p)lay again or (q)uit?"
+    start_game
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -19,9 +19,25 @@ RSpec.describe Game do
     expect(game.quit).to eq("You have quit Mastermind")
   end
 
-  it 'has a start' do
+  it 'has a game start' do
     game = Game.new
-    expect(game.start_game).to be(user_input)
+    expect(game.start_game).to eq(@user_input)
+  end
+
+  it 'has a start time' do
+    game = Game.new
+    expect(game.set_start_time).to eq(@start_time)
+  end
+
+  it 'has an end time' do
+    game = Game.new
+    expect(game.set_end_time).to eq(@end_time)
+  end
+
+  it 'can calculate the length of a game' do
+    game = Game.new
+    expect(game.calculate_game_time).to eq(@game_time)
+  end
 
   it 'sets a secret code' do
     game = Game.new
@@ -33,8 +49,15 @@ RSpec.describe Game do
     expect(game.cheat).to eq(@secret)
   end
 
+  it 'counts the number of guesses' do
+    game = Game.new
+    expect(game.guess).to eq(0)
+    game.add_guess
+    expect(game.guess).to eq(1)
+  end
+
   it 'has an end' do
     game = Game.new
-    expect(game.end_game).to eq(end_game_message)
+    expect(game.end_game).to eq(game.end_game_message)
   end
 end
