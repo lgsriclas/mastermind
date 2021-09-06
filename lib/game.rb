2e884be84fc85693f946262d8160d1507ace05b7
Stephanie Helm <stephaniemhelm@gmail.com>
2021-09-06 16:47:57 -0600

    refactor

--- lib/game.rb
+++ lib/game.rb
@@ -61,7 +61,7 @@ class Game
 
     p  "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game. What's your guess?"
     @player_guess = gets.chomp.downcase
-    require "pry"; binding.pry
+
   end
 
 
