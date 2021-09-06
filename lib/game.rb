906d3de4b7369e9518cdc1f932428b108cd529ca
Lesley Sanders <lesleyasanders@gmail.com>
2021-09-06 08:35:12 -0400

    Fix: update game

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 require './runner/mastermind'
 require './lib/secret'
-require './lib/turn'
+#require './lib/turn'
 
 class Game
   attr_reader :turn_counter, :secret, :user_input, :start_time, :end_time, :player_guess, :game_time, :player_input
@@ -22,6 +22,12 @@ class Game
     "You have quit Mastermind"
   end
 
+  def set_secret_code
+    @secret = Secret.new
+    secret.generate_secret_code
+    require "pry"; binding.pry
+  end
+
   def start_game
     player_input = gets.chomp.downcase!
     if player_input == "p"
@@ -36,6 +42,10 @@ class Game
       p cheat
     end
     @player_input
+  end
+
+  def cheat
+    secret.set_secret_code
   end
 
   def set_start_time
@@ -56,15 +66,6 @@ class Game
     set_start_time
     #turn = Turn.new
     p  "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game. What's your guess?"
-  end
-
-  def set_secret_code
-    @secret = Secret.new
-    secret.generate_secret_code
-  end
-
-  def cheat
-    @secret.secret_code
   end
 
   def guess
